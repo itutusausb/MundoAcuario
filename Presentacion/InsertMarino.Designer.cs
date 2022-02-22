9818009ab4aa8aa1245d787f38832b93593c930f
Laboratorio <Laboratorio@LAB-FIRMWARE03>
2022-02-22 14:32:22 +0100

    UX registro marino

--- Presentacion/InsertMarino.Designer.cs
+++ Presentacion/InsertMarino.Designer.cs
@@ -32,13 +32,13 @@ namespace MundoAcuario.Presentacion
             this.label1 = new System.Windows.Forms.Label();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.txtca = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtal = new System.Windows.Forms.TextBox();
+            this.txtsal = new System.Windows.Forms.TextBox();
+            this.txttemp = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.txtmg = new System.Windows.Forms.TextBox();
             this.textBox7 = new System.Windows.Forms.TextBox();
-            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.txtamo = new System.Windows.Forms.TextBox();
             this.textBox9 = new System.Windows.Forms.TextBox();
             this.textBox10 = new System.Windows.Forms.TextBox();
             this.textBox11 = new System.Windows.Forms.TextBox();
@@ -46,6 +46,14 @@ namespace MundoAcuario.Presentacion
             this.textBox13 = new System.Windows.Forms.TextBox();
             this.textBox14 = new System.Windows.Forms.TextBox();
             this.textBox15 = new System.Windows.Forms.TextBox();
+            this.labelca = new System.Windows.Forms.Label();
+            this.labelal = new System.Windows.Forms.Label();
+            this.labelsal = new System.Windows.Forms.Label();
+            this.labeltemp = new System.Windows.Forms.Label();
+            this.labelph = new System.Windows.Forms.Label();
+            this.labelmg = new System.Windows.Forms.Label();
+            this.labelfos = new System.Windows.Forms.Label();
+            this.labelamo = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -61,124 +69,201 @@ namespace MundoAcuario.Presentacion
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(150, 92);
+            this.dateTimePicker1.Location = new System.Drawing.Point(50, 95);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
             this.dateTimePicker1.TabIndex = 1;
             // 
             // txtca
             // 
-            this.txtca.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtca.Location = new System.Drawing.Point(340, 177);
+            this.txtca.Location = new System.Drawing.Point(50, 144);
             this.txtca.Name = "txtca";
-            this.txtca.Size = new System.Drawing.Size(142, 13);
+            this.txtca.Size = new System.Drawing.Size(142, 20);
             this.txtca.TabIndex = 2;
-            this.txtca.Text = "CA";
-            this.txtca.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // textBox2
+            // txtal
             // 
-            this.textBox2.Location = new System.Drawing.Point(150, 159);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 3;
+            this.txtal.Location = new System.Drawing.Point(50, 197);
+            this.txtal.Name = "txtal";
+            this.txtal.Size = new System.Drawing.Size(142, 20);
+            this.txtal.TabIndex = 3;
             // 
-            // textBox3
+            // txtsal
             // 
-            this.textBox3.Location = new System.Drawing.Point(150, 185);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 4;
+            this.txtsal.Location = new System.Drawing.Point(50, 249);
+            this.txtsal.Name = "txtsal";
+            this.txtsal.Size = new System.Drawing.Size(142, 20);
+            this.txtsal.TabIndex = 4;
             // 
-            // textBox4
+            // txttemp
             // 
-            this.textBox4.Location = new System.Drawing.Point(150, 211);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 5;
+            this.txttemp.Location = new System.Drawing.Point(50, 301);
+            this.txttemp.Name = "txttemp";
+            this.txttemp.Size = new System.Drawing.Size(142, 20);
+            this.txttemp.TabIndex = 5;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(150, 237);
+            this.textBox5.Location = new System.Drawing.Point(50, 353);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.Size = new System.Drawing.Size(142, 20);
             this.textBox5.TabIndex = 6;
             // 
-            // textBox6
+            // txtmg
             // 
-            this.textBox6.Location = new System.Drawing.Point(150, 263);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 20);
-            this.textBox6.TabIndex = 7;
+            this.txtmg.Location = new System.Drawing.Point(50, 405);
+            this.txtmg.Name = "txtmg";
+            this.txtmg.Size = new System.Drawing.Size(142, 20);
+            this.txtmg.TabIndex = 7;
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(150, 289);
+            this.textBox7.Location = new System.Drawing.Point(366, 144);
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.Size = new System.Drawing.Size(142, 20);
             this.textBox7.TabIndex = 8;
             // 
-            // textBox8
+            // txtamo
             // 
-            this.textBox8.Location = new System.Drawing.Point(150, 315);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(100, 20);
-            this.textBox8.TabIndex = 9;
+            this.txtamo.Location = new System.Drawing.Point(366, 197);
+            this.txtamo.Name = "txtamo";
+            this.txtamo.Size = new System.Drawing.Size(142, 20);
+            this.txtamo.TabIndex = 9;
             // 
             // textBox9
             // 
-            this.textBox9.Location = new System.Drawing.Point(150, 341);
+            this.textBox9.Location = new System.Drawing.Point(366, 249);
             this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.Size = new System.Drawing.Size(142, 20);
             this.textBox9.TabIndex = 10;
             // 
             // textBox10
             // 
-            this.textBox10.Location = new System.Drawing.Point(150, 367);
+            this.textBox10.Location = new System.Drawing.Point(366, 301);
             this.textBox10.Name = "textBox10";
-            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.Size = new System.Drawing.Size(142, 20);
             this.textBox10.TabIndex = 11;
             // 
             // textBox11
             // 
-            this.textBox11.Location = new System.Drawing.Point(150, 393);
+            this.textBox11.Location = new System.Drawing.Point(366, 353);
             this.textBox11.Name = "textBox11";
-            this.textBox11.Size = new System.Drawing.Size(100, 20);
+            this.textBox11.Size = new System.Drawing.Size(142, 20);
             this.textBox11.TabIndex = 12;
             // 
             // textBox12
             // 
-            this.textBox12.Location = new System.Drawing.Point(150, 419);
+            this.textBox12.Location = new System.Drawing.Point(366, 405);
             this.textBox12.Name = "textBox12";
-            this.textBox12.Size = new System.Drawing.Size(100, 20);
+            this.textBox12.Size = new System.Drawing.Size(142, 20);
             this.textBox12.TabIndex = 13;
             // 
             // textBox13
             // 
-            this.textBox13.Location = new System.Drawing.Point(150, 445);
+            this.textBox13.Location = new System.Drawing.Point(649, 144);
             this.textBox13.Name = "textBox13";
-            this.textBox13.Size = new System.Drawing.Size(100, 20);
+            this.textBox13.Size = new System.Drawing.Size(142, 20);
             this.textBox13.TabIndex = 14;
             // 
             // textBox14
             // 
-            this.textBox14.Location = new System.Drawing.Point(150, 471);
+            this.textBox14.Location = new System.Drawing.Point(649, 197);
             this.textBox14.Name = "textBox14";
-            this.textBox14.Size = new System.Drawing.Size(100, 20);
+            this.textBox14.Size = new System.Drawing.Size(142, 20);
             this.textBox14.TabIndex = 15;
             // 
             // textBox15
             // 
-            this.textBox15.Location = new System.Drawing.Point(150, 497);
+            this.textBox15.Location = new System.Drawing.Point(649, 249);
             this.textBox15.Name = "textBox15";
-            this.textBox15.Size = new System.Drawing.Size(100, 20);
+            this.textBox15.Size = new System.Drawing.Size(142, 20);
             this.textBox15.TabIndex = 16;
+            // 
+            // labelca
+            // 
+            this.labelca.AutoSize = true;
+            this.labelca.Location = new System.Drawing.Point(47, 128);
+            this.labelca.Name = "labelca";
+            this.labelca.Size = new System.Drawing.Size(39, 13);
+            this.labelca.TabIndex = 17;
+            this.labelca.Text = "Calcio:";
+            // 
+            // labelal
+            // 
+            this.labelal.AutoSize = true;
+            this.labelal.Location = new System.Drawing.Point(47, 181);
+            this.labelal.Name = "labelal";
+            this.labelal.Size = new System.Drawing.Size(61, 13);
+            this.labelal.TabIndex = 18;
+            this.labelal.Text = "Alcalinidad:";
+            // 
+            // labelsal
+            // 
+            this.labelsal.AutoSize = true;
+            this.labelsal.Location = new System.Drawing.Point(47, 233);
+            this.labelsal.Name = "labelsal";
+            this.labelsal.Size = new System.Drawing.Size(53, 13);
+            this.labelsal.TabIndex = 19;
+            this.labelsal.Text = "Salinidad:";
+            // 
+            // labeltemp
+            // 
+            this.labeltemp.AutoSize = true;
+            this.labeltemp.Location = new System.Drawing.Point(47, 285);
+            this.labeltemp.Name = "labeltemp";
+            this.labeltemp.Size = new System.Drawing.Size(70, 13);
+            this.labeltemp.TabIndex = 20;
+            this.labeltemp.Text = "Temperatura:";
+            // 
+            // labelph
+            // 
+            this.labelph.AutoSize = true;
+            this.labelph.Location = new System.Drawing.Point(47, 337);
+            this.labelph.Name = "labelph";
+            this.labelph.Size = new System.Drawing.Size(23, 13);
+            this.labelph.TabIndex = 21;
+            this.labelph.Text = "Ph:";
+            // 
+            // labelmg
+            // 
+            this.labelmg.AutoSize = true;
+            this.labelmg.Location = new System.Drawing.Point(47, 389);
+            this.labelmg.Name = "labelmg";
+            this.labelmg.Size = new System.Drawing.Size(56, 13);
+            this.labelmg.TabIndex = 22;
+            this.labelmg.Text = "Magnesio:";
+            // 
+            // labelfos
+            // 
+            this.labelfos.AutoSize = true;
+            this.labelfos.Location = new System.Drawing.Point(363, 128);
+            this.labelfos.Name = "labelfos";
+            this.labelfos.Size = new System.Drawing.Size(45, 13);
+            this.labelfos.TabIndex = 23;
+            this.labelfos.Text = "Fosfato:";
+            // 
+            // labelamo
+            // 
+            this.labelamo.AutoSize = true;
+            this.labelamo.Location = new System.Drawing.Point(363, 181);
+            this.labelamo.Name = "labelamo";
+            this.labelamo.Size = new System.Drawing.Size(57, 13);
+            this.labelamo.TabIndex = 24;
+            this.labelamo.Text = "Amoniaco:";
             // 
             // InsertMarino
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(958, 534);
+            this.Controls.Add(this.labelamo);
+            this.Controls.Add(this.labelfos);
+            this.Controls.Add(this.labelmg);
+            this.Controls.Add(this.labelph);
+            this.Controls.Add(this.labeltemp);
+            this.Controls.Add(this.labelsal);
+            this.Controls.Add(this.labelal);
+            this.Controls.Add(this.labelca);
             this.Controls.Add(this.textBox15);
             this.Controls.Add(this.textBox14);
             this.Controls.Add(this.textBox13);
@@ -186,17 +271,18 @@ namespace MundoAcuario.Presentacion
             this.Controls.Add(this.textBox11);
             this.Controls.Add(this.textBox10);
             this.Controls.Add(this.textBox9);
-            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.txtamo);
             this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.txtmg);
             this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txttemp);
+            this.Controls.Add(this.txtsal);
+            this.Controls.Add(this.txtal);
             this.Controls.Add(this.txtca);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.label1);
             this.Name = "InsertMarino";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "InsertMarino";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -208,13 +294,13 @@ namespace MundoAcuario.Presentacion
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.TextBox txtca;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtal;
+        private System.Windows.Forms.TextBox txtsal;
+        private System.Windows.Forms.TextBox txttemp;
         private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox txtmg;
         private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox txtamo;
         private System.Windows.Forms.TextBox textBox9;
         private System.Windows.Forms.TextBox textBox10;
         private System.Windows.Forms.TextBox textBox11;
@@ -222,5 +308,13 @@ namespace MundoAcuario.Presentacion
         private System.Windows.Forms.TextBox textBox13;
         private System.Windows.Forms.TextBox textBox14;
         private System.Windows.Forms.TextBox textBox15;
+        private System.Windows.Forms.Label labelca;
+        private System.Windows.Forms.Label labelal;
+        private System.Windows.Forms.Label labelsal;
+        private System.Windows.Forms.Label labeltemp;
+        private System.Windows.Forms.Label labelph;
+        private System.Windows.Forms.Label labelmg;
+        private System.Windows.Forms.Label labelfos;
+        private System.Windows.Forms.Label labelamo;
     }
 }
